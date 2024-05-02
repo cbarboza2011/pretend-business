3ffd64d841f4b2373bf0c98fdaa0f68eab0e0384
Cristiana Barboza <50346228+cbarboza2011@users.noreply.github.com>
2024-05-02 11:04:03 -0500

    fix stylesheet for moble-first

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,14 @@
 $(document).ready(function() {
+
+  // OPEN NAV MENU ON MOBILE
+  $('#navbutton').on('click', function() {
+    if ($('#navlist').is(':visible')) {
+      $('#navlist').slideToggle();
+      $('#navbutton').text('☰');
+    } else {
+      $('#navlist').slideToggle();
+      $('#navbutton').text('X');
+    }
+  });
+
 });
